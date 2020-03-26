07f728041af1bedc9c660557753dca015ae4d19e
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-03-26 23:32:29 +0700

    a

--- php/db.php
+++ php/db.php
@@ -1,6 +1,6 @@
 <?php
-$c = new mysqli("localhost", "u117143075_food", "HaloDunia123");
-$c->select_db("u117143075_food");
+$c = new mysqli("localhost", "osge9971_fdelivery", "HaloDunia123");
+$c->select_db("osge9971_fdelivery");
 
 /*$c = new mysqli("localhost", "fdelivery", "HaloDunia123");
 $c->select_db("fdelivery");*/
